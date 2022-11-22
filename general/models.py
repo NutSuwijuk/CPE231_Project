c015b62bba2f0cabdfd13c6022f210c45557e1d0
MinsPP <supakorn.srikong@gmail.com>
2022-11-22 20:55:12 +0700

    model

--- general/models.py
+++ general/models.py
@@ -13,7 +13,7 @@ class Stock(models.Model):
         db_table = "stock"
         managed = False
     def __str__(self):
-        return self.stock
+        return self.id_stock
 
 # class Ingredient(models.Model):
 #     code = models.CharField(max_length=10,primary_key=True)
@@ -26,43 +26,57 @@ class Stock(models.Model):
 #     def __str__(self):
 #         return self.code
 
-class Customer(models.Model):
-    customer_code = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=100, null=True)
-    address = models.CharField(max_length=100, null=True, blank=True)
-    credit_limit = models.FloatField(null=True, blank=True)
-    country = models.CharField(max_length=20, null=True, blank=True)
+class Menu(models.Model):
+    menu_id = models.CharField(max_length=10, primary_key=True)
+    menu_name = models.CharField(max_length=100, null=True)
+    price = models.FloatField(null=True, blank=True)
+    amount_of_coffee = models.IntegerField(null=True)
+    amount_of_milk = models.IntegerField(null=True)
+    amount_of_chocolate = models.IntegerField(null=True)
+    amount_of_syrub = models.IntegerField(null=True)
     class Meta:
-        db_table = "customer"
+        db_table = "menu"
         managed = False
     def __str__(self):
-        return self.customer_code
+        return self.menu_id
 
-class Invoice(models.Model):
-    invoice_no = models.CharField(max_length=10, primary_key=True)
-    date = models.DateField(null=True)
-    customer_code = models.ForeignKey(Customer, on_delete=models.CASCADE, db_column='customer_code')
-    due_date = models.DateField(null=True, blank=True)
-    total = models.FloatField(null=True, blank=True)
-    vat = models.FloatField(null=True, blank=True)
-    amount_due = models.FloatField(null=True, blank=True)
+class AdditionalItems(models.Model):
+    type = models.CharField(max_length=10, primary_key=True)
+    description = models.CharField(max_length=100, null=True)
+    mash = models.IntegerField(null=True)
     class Meta:
-        db_table = "invoice"
+        db_table = "additional_items"
         managed = False
     def __str__(self):
-        return self.invoice_no
+        return self.type
 
-class InvoiceLineItem(models.Model):
-    invoice_no = models.ForeignKey(Invoice, on_delete=models.CASCADE, db_column='invoice_no')
-    item_no = models.IntegerField()
-    product_code = models.ForeignKey(Product, on_delete=models.CASCADE, db_column='product_code')
-    quantity = models.IntegerField(null=True)
-    unit_price = models.FloatField(null=True)
-    product_total = models.FloatField(null=True)
+class Sweet(models.Model):
+    sweet_level = models.CharField(max_length=10, primary_key=True)
+    description = models.CharField(max_length=100, null=True)
     class Meta:
-        db_table = "invoice_line_item"
-        unique_together = ("invoice_no", "item_no")
+        db_table = "sweet"
         managed = False
     def __str__(self):
-        return '{"invoice_no":"%s","item_no":"%s"}' % (self.invoice_no, self.item_no)
-        #return '{"invoice_no":"%s","item_no":"%s","product_code":"%s","product_name":"%s","quantity":%s,"unit_price":"%s","product_total":"%s"}' % (self.invoice_no, self.item_no, self.product_code, self.product_code.name, self.quantity, self.unit_price, self.product_total)
+        return self.sweet_level
+
+class PaymentMethod(models.Model):
+    payment_method = models.CharField(max_length=10,primary_key=True)
+    description = models.CharField(max_length=100, null=True)
+    class Meta:
+        db_table = "payment_method"
+        managed = False
+    def __str__(self):
+        return self.payment_method
+
+class Users(models.Model):
+    id_user = models.CharField(max_length=10,primary_key=True)
+    username = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length=100, null=True)
+    phone_number = models.IntegerField(null=True)
+    password = models.CharField(max_length=10)
+    point = models.IntegerField(null=True)
+    class Meta:
+        db_table = "users"
+        managed = False
+    def __str__(self):
+        return self.id_user
