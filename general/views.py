1b113eec699b6e62bc64023b4434616a720da944
MinsPP <supakorn.srikong@gmail.com>
2022-11-22 19:58:50 +0700

    edit model,views.setting

--- general/views.py
+++ general/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.views.generic import View
+from django.http import JsonResponse
+from django import forms
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.forms.models import model_to_dict
+from django.db.models import Max
+from django.db import transaction
+from .models import *
+import json
+import re
 
 # Create your views here.
+def index(request):
+    data = {}
+    return render(request, 'general/index.html', data)
+
+class StockList(View):
+    def get(self, request):
+        products = list(Stock.objects.all().values())
+        data = dict()
+        data['products'] = products
+
+        return JsonResponse(data)
+
+class CustomerList(View):
+    def get(self, request):
+        customers = list(Customer.objects.all().values())
+        data = dict()
+        data['customers'] = customers
+
+        return JsonResponse(data)
