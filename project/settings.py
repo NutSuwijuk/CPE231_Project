6ea33ee3f3a307c8d56f175ef02af0ff9ee872b0
NutSuwijuk <suwijuk2261@gmail.com>
2022-11-23 12:44:36 +0700

    Login Logout

--- project/settings.py
+++ project/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Auth
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
